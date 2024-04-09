993832d21735d59ca072bd551e2750021c9a0c85
Dominicstgo <142622439+Dominicstgo@users.noreply.github.com>
2024-04-09 12:03:41 -0700

    Removed unused libraries. made strings const. optimized vector 3 math.

--- Prototype4/Assets/Scripts/Enemy.cs
+++ Prototype4/Assets/Scripts/Enemy.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 3.0f;
+    [SerializeField] private float speed = 3.0f;
     private Rigidbody enemyRb;
     private GameObject player;
 
@@ -20,7 +18,7 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = player.transform.position - transform.position;
         lookDirection.Normalize();
-        enemyRb.AddForce(lookDirection * speed);
+        enemyRb.AddForce(lookDirection (* speed));
         if(transform.position.y < -10)
         {
             Destroy(gameObject);
